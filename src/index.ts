4c5949584e77623aada32256353a59afad53fba3
tmkn <tuomasklein@gmail.com>
2021-01-13 16:03:56 +0100

    converted MagicLoginStrategy to use class keyword

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import type { StrategyCreatedStatic } from 'passport';
 import { generateToken, decodeToken } from './token';
 
 type VerifyCallback = (
@@ -18,80 +19,70 @@ interface Options {
   verify: VerifyCallback;
 }
 
-declare class MagicLoginStrategy {
-  constructor(options: Options);
-  authenticate(req: Request): void;
-  confirm(req: Request, res: Response): void;
-  send(req: Request, res: Response): void;
-}
+class MagicLoginStrategy {
+  name: string = 'magiclogin';
 
-function MagicLoginStrategy(options: Options) {
-  this.name = 'magiclogin';
-  this.callbackUrl = options.callbackUrl;
-  this.confirmUrl = options.confirmUrl;
-  this._options = options;
-  this.send = this.send.bind(this);
-  this.confirm = this.confirm.bind(this);
-}
+  constructor(private _options: Options) { }
 
-MagicLoginStrategy.prototype.authenticate = function(req) {
-  const self = this;
-  const payload = decodeToken({
-    secret: self._options.secret,
-    token: req.query.token as string,
-  });
-  const verifyCallback = function(err?: Error, user?: Object, info?: any) {
-    if (err) {
-      return self.error(err);
-    } else if (!user) {
-      return self.fail(info);
-    } else {
-      return self.success(user, info);
-    }
-  };
-
-  self._options.verify(payload, verifyCallback);
-};
-
-MagicLoginStrategy.prototype.send = function(req: Request, res: Response) {
-  if (!req.body.destination) {
-    res.status(400).send('Please specify the destination.');
-    return;
-  }
-
-  const code = Math.floor(Math.random() * 90000) + 10000 + '';
-  const jwt = generateToken({
-    secret: this._options.secret,
-    destination: req.body.destination,
-    code,
-  });
-
-  this._options
-    .sendMagicLink(
-      req.body.destination,
-      `${this._options.confirmUrl}?token=${jwt}`,
-      code
-    )
-    .then(() => {
-      res.json({ success: true, code });
-    })
-    .catch((error: any) => {
-      console.error(error);
-      res.json({ success: false, error });
+  authenticate(this: StrategyCreatedStatic & MagicLoginStrategy, req: Request): void {
+    const self = this;
+    const payload = decodeToken({
+      secret: self._options.secret,
+      token: req.query.token as string,
     });
-};
-
-MagicLoginStrategy.prototype.confirm = function(req: Request, res: Response) {
-  const data = decodeToken({
-    token: req.query.token as string,
-    secret: this._options.secret,
-  });
-
-  if (data) {
-    res.redirect(`${this._options.callbackUrl}?token=${req.query.token}`);
-  } else {
-    res.send('Expired login link. Please try again!');
+    const verifyCallback = function(err?: Error, user?: Object, info?: any) {
+      if (err) {
+        return self.error(err);
+      } else if (!user) {
+        return self.fail(info);
+      } else {
+        return self.success(user, info);
+      }
+    };
+  
+    self._options.verify(payload, verifyCallback);
   }
-};
+
+  send = (req: Request, res: Response): void => {
+    if (!req.body.destination) {
+      res.status(400).send('Please specify the destination.');
+      return;
+    }
+  
+    const code = Math.floor(Math.random() * 90000) + 10000 + '';
+    const jwt = generateToken({
+      secret: this._options.secret,
+      destination: req.body.destination,
+      code,
+    });
+  
+    this._options
+      .sendMagicLink(
+        req.body.destination,
+        `${this._options.confirmUrl}?token=${jwt}`,
+        code
+      )
+      .then(() => {
+        res.json({ success: true, code });
+      })
+      .catch((error: any) => {
+        console.error(error);
+        res.json({ success: false, error });
+      });
+  }
+
+  confirm = (req: Request, res: Response): void => {
+    const data = decodeToken({
+      token: req.query.token as string,
+      secret: this._options.secret,
+    });
+  
+    if (data) {
+      res.redirect(`${this._options.callbackUrl}?token=${req.query.token}`);
+    } else {
+      res.send('Expired login link. Please try again!');
+    }
+  }
+}
 
 export default MagicLoginStrategy;
